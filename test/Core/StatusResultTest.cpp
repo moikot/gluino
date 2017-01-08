942b3c43276c2d92dce65cbbe3d6e3c89613277a
Anisimov Sergey <anisimov.sergey@gmail.com>
2017-01-08 23:21:13 +0000

    Adding tests for StatusResult

--- test/Core/StatusResultTest.cpp
+++ test/Core/StatusResultTest.cpp
@@ -5,6 +5,26 @@
 
 using namespace Core;
 
+TEST(StatusResult, Is_OK_When_Status_Code_Is_OK) {
+  auto result = StatusResult::makeUnique(StatusCode::OK, "test");
+  ASSERT_TRUE(result->isOk());
+}
+
+TEST(StatusResult, Is_Not_OK_When_Status_Code_Is_Not_OK) {
+  auto result = StatusResult::makeUnique(StatusCode::Accepted, "test");
+  ASSERT_FALSE(result->isOk());
+}
+
+TEST(StatusResult, Is_Accepted_When_Status_Code_Is_Accepted) {
+  auto result = StatusResult::makeUnique(StatusCode::Accepted, "test");
+  ASSERT_TRUE(result->isAccepted());
+}
+
+TEST(StatusResult, Is_Not_Accepted_When_Status_Code_Is_Not_Accepted) {
+  auto result = StatusResult::makeUnique(StatusCode::OK, "test");
+  ASSERT_FALSE(result->isAccepted());
+}
+
 TEST(StatusResult, Status_Code_Retained) {
   auto result = StatusResult::makeUnique(StatusCode::OK, "test");
   ASSERT_EQ(result->getStatusCode(), StatusCode::OK);
@@ -13,4 +33,16 @@ TEST(StatusResult, Status_Code_Retained) {
 TEST(StatusResult, Message_Retained) {
   auto result = StatusResult::makeUnique(StatusCode::OK, "test");
   ASSERT_EQ(result->getMessage(), "test");
+}
+
+TEST(StatusResult, Inner_Result_Is_Null_By_Default) {
+  auto result = StatusResult::makeUnique(StatusCode::OK, "test");
+  ASSERT_EQ(result->getInnerReuslt(), nullptr);
+}
+
+TEST(StatusResult, Inner_Result_Is_Retained) {
+  auto inner = StatusResult::makeUnique(StatusCode::OK, "test");
+  auto innerPtr = inner.get();
+  auto result = StatusResult::makeUnique(StatusCode::OK, "test", std::move(inner));
+  ASSERT_EQ(result->getInnerReuslt(), innerPtr);
 }
