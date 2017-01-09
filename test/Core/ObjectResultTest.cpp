bc903d7d9fe529feb8909777a2ce7716a5dd7c32
Sergey Anisimov <anisimov.sergey@gmail.com>
2017-01-09 23:05:03 +0000

    Adding unit tests for ObjectResult

---
+++ test/Core/ObjectResultTest.cpp
@@ -0,0 +1,20 @@
+
+#include <gtest/gtest.h>
+
+#include "Core/StatusResult.hpp"
+#include "Core/ObjectResult.hpp"
+
+using namespace Core;
+
+TEST(ObjectResult, Status_Code_Retained) {
+  auto obj = StatusResult::makeUnique(StatusCode::OK, "test");
+  auto result = ObjectResult::makeUnique(StatusCode::OK, std::move(obj));
+  ASSERT_EQ(result->getStatusCode(), StatusCode::OK);
+}
+
+TEST(ObjectResult, Object_Retained) {
+  auto obj = StatusResult::makeUnique(StatusCode::OK, "test");
+  auto objPtr = obj.get();
+  auto result = ObjectResult::makeUnique(StatusCode::OK, std::move(obj));
+  ASSERT_EQ(&result->getObject(), objPtr);
+}
